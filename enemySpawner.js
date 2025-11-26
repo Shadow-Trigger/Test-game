2c460582a5d257cc9e2e3dc03495e532a19552ad
Shadow-Trigger <aoreilly30@belvederecollege.ie>
2025-11-26 16:08:34 +0000

    Update enemySpawner.js

--- enemySpawner.js
+++ enemySpawner.js
@@ -2,8 +2,8 @@
 
 // Enemy types
 export const enemyTypes = {
-  normal: { hp: 40, speed: 2, color: "red" },
-  fast: { hp: 50, speed: 5, color: "purple" }
+  normal: { hp: 50, speed: 1.75, color: "red" },
+  fast: { hp: 30, speed: 5, color: "purple" }
 };
 
 // Spawn enemy and increment enemiesAlive
