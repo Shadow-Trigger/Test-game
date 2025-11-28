c2eb16b71e6ea8224c6a30210486f7d0b4eac4e7
Shadow-Trigger <aoreilly30@belvederecollege.ie>
2025-11-28 11:45:58 +0000

    Update index.js

--- Enemies/index.js
+++ Enemies/index.js
@@ -1,1 +1,14 @@
+import { createNormalEnemy, updateNormalEnemy } from "./NormalEnemy.js";
+import { createFastEnemy, updateFastEnemy } from "./FastEnemy.js";
+
+export const enemyTypes = {
+  normal: {
+    create: createNormalEnemy,
+    update: updateNormalEnemy
+  },
+  fast: {
+    create: createFastEnemy,
+    update: updateFastEnemy
+  }
+};
 
